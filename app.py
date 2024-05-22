0944c0b90bc1cf028436dd0764804afbf947a6ef
M Shakila <mshakila.she@gmail.com>
2024-05-22 15:31:52 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "Welcome to Flask"
+
+@app.route("/calculator", methods=["GET"])
+def math_operator():
+    operation=request.json["operation"]   # requesting from postman in form of json
+    num1=request.json["num1"]
+    num2=request.json["num2"]
+
+    if operation=="add":
+        result = (num1 + num2)
+    elif operation == "subtract":
+        result = num1 - num2
+    elif operation == "multiply":
+        result = num1 * num2
+    elif operation == "divide"
+        result = num1 / num2
+    else:
+        print("check the numbers")
+    return result
+
+
+
+print(__name__)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
