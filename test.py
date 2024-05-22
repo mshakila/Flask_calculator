59168ac50cde640848933582c46e3d94c0cc68da
M Shakila <mshakila.she@gmail.com>
2024-05-22 17:16:29 +0530

    test.json added

--- test.py
+++ test.py
@@ -2,36 +2,20 @@ from flask import Flask, request, json, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def welcome():
-    return "Welcome to Flask"
+@app.route("/cal", methods=["GET"])
+def math_operation():
+    operation = request.json["operation"]
+    num1 = request.json["num1"]
+    num2 = request.json["num2"]
+    num3 = request.json["num3"]
 
-@app.route("/calculator", methods=["GET"])
-def math_operator():
-    operation=request.json["operation"]   # requesting from postman in form of json
-    num1=request.json["num1"]
-    num2=request.json["num2"]
-
-    if operation=="add":
-        result = (num1 + num2)
-    elif operation == "subtract":
-        result = num1 - num2
+    if operation == "add":
+        result = num1 + num2 + num3
     elif operation == "multiply":
-        result = num1 * num2
-    elif operation == "divide":
-        result = num1 / num2
-    elif operation == "reminder":
-        result = num1 % num2
-    elif operation == "floor":
-        result = num1 //num2
+        result = num1 * num2 * num3
     else:
-        return "check the operation given"
-    
-    return "The operation is {} and the result is {}".format(operation,result)
-    #return jsonify(result)
-
-
-print(__name__)
+        return "check the operation"
+    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(debug=True)
